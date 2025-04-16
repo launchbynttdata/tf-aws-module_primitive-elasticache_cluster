1df61ae5e5654ef14b9dff239088451a02692a14
Angel Velasquez <Angel.Velasquez@nttdata.com>
2025-04-16 10:35:07 -0600

    fix: add aws provider version constraint

--- examples/basic/versions.tf
+++ examples/basic/versions.tf
@@ -14,9 +14,9 @@ terraform {
   required_version = "~> 1.0"
 
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.14"
     }
   }
 }
