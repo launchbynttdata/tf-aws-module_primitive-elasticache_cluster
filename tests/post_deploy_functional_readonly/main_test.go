d7d6ab39be79744de92fff27a794fa2b88759242
Ben Vaughan <160539772+ben-vaughan-nttd@users.noreply.github.com>
2026-08-28 11:41:10 -0500

    fix: consolidate Dependabot updates and Terratest 1.x (#15)
    * fix: consolidate Dependabot updates and Terratest 1.x
    
    * fix: CI lint config and Makefile guard
    
    * fix: CI lint config and Makefile guard
    
    * fix: CI lint config and Makefile guard
    
    * fix: commit tooling dotfiles and restore tflint ruleset

--- tests/post_deploy_functional_readonly/main_test.go
+++ tests/post_deploy_functional_readonly/main_test.go
@@ -33,5 +33,5 @@ func TestElasticacheClusterModule(t *testing.T) {
 		SetTestConfigFileName(infraTFVarFileNameDefault).
 		Build()
 
-	lib.RunSetupTestTeardown(t, *ctx, testimpl.TestComposableComplete)
+	lib.RunNonDestructiveTest(t, *ctx, testimpl.TestComposableComplete)
 }
